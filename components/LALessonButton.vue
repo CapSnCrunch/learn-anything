a420df8a76a1ccbeb0dd5a5c3b9c0d029e971f96
Samuel Perales <samuelperales4@gmail.com>
2024-04-07 22:54:29 -0500

    setup course sections with less buttons

---
+++ components/LALessonButton.vue
@@ -0,0 +1,72 @@
+<template>
+  <div
+    class="d-flex align-center justify-center text-decoration-none position-relative"
+  >
+    <div
+      class="button ma-0 align-center justify-center position-absolute"
+      style="width: 70px; height: 60px"
+      :style="{ 'background-color': computedColor }"
+    >
+      <div class="text-eel">
+        <v-icon icon="mdi-star-four-points" size="35px" color="white" />
+      </div>
+    </div>
+    <div
+      class="button-side"
+      style="width: 70px; height: 60px"
+      :style="{ 'background-color': computedColor }"
+    >
+      <div
+        class="button-side"
+        style="width: 70px; height: 60px"
+        :style="{ 'background-color': 'rgba(0, 0, 0, 0.3)' }"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, computed } from "vue";
+
+const props = defineProps({
+  color: {
+    type: String,
+    default: "#1cb0f6",
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  complete: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const computedColor = computed(() => {
+  if (props.complete) {
+    return "#FEBB3E";
+  }
+  return props.disabled ? "#AFAFAF" : props.color;
+});
+</script>
+
+<style scoped>
+.button {
+  border-radius: 50%;
+  transition: all 0.1s;
+  display: inline-flex;
+  padding: 12px 16px;
+  cursor: pointer;
+  bottom: 7px;
+}
+
+.button:active {
+  bottom: 0px;
+}
+
+.button-side {
+  border-radius: 50%;
+  transition: all 0.1s;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,8 @@
     <v-row class="d-flex flex-column align-center justify-center">
       <v-col class="pa-0 ma-0">
         <v-sheet
-          class="section d-flex align-center"
-          style="padding-bottom: 200px"
+          class="d-flex align-center justify-center"
+          style="padding-bottom: 200px; height: 100vh"
         >
           <v-col cols="6" class="d-flex justify-end align-center pr-10">
             <img src="../assets/login-image.png" width="600px" height="600px" />
@@ -34,15 +34,3 @@
 <script setup>
 import LAButton from "@/components/LAButton.vue";
 </script>
-
-<style scoped>
-.section {
-  height: 100vh;
-  display: flex !important;
-  align-items: start;
-  justify-content: center;
-  text-align: center;
-  font-size: 24px;
-  position: relative;
-}
-</style>
